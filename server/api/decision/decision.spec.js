9b350c00e20119306c3b97dd6177291dcfe31813
Gerardo Zenobi <gerardo.zenobi@sparkventures.co.nz>
2015-07-10 16:31:56 +1200

    Starting to unit test...

--- server/api/decision/decision.spec.js
+++ server/api/decision/decision.spec.js
@@ -4,16 +4,23 @@ var should = require('should');
 var app = require('../../app');
 var request = require('supertest');
 
-describe('GET /api/alive', function() {
+describe('POST /api/decision/runDecisionSimulation', function() {
 
-  it('should respond with JSON array', function(done) {
+  it('should respond with results array', function(done) {
     request(app)
-      .get('/api/alive')
+      .post('/api/decision/runDecisionSimulation')
+      .send({
+        'items': [{
+          name: "blue",
+          weight: 23
+        }]
+      })
       .expect(200)
       .expect('Content-Type', /json/)
       .end(function(err, res) {
         if (err) return done(err);
-        //res.body.should.be.instanceof(Array);
+        res.body.results.should.be.instanceof(Array);
+        console.log(res.body);
         done();
       });
   });
